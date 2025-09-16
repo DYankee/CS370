6e7c6c1c5f72ddc551961b016be2324a423c87ef
Zachary <zpgeary@gmail.com>
2025-09-16 15:09:47 -0400

    Added basic program to open a window and display text

--- CS370/CS370.cpp
+++ CS370/CS370.cpp
@@ -7,6 +7,21 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	const int screenWidth = 800;
+	const int screenHeight = 600;
+	// Create the main window
+	
+	InitWindow(screenWidth, screenHeight, "CS370");
+
+	while (!WindowShouldClose()) // Detect window close button or ESC key
+	{
+		BeginDrawing();
+
+		ClearBackground(RAYWHITE);
+
+		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+		EndDrawing();
+	}
 	return 0;
 }
