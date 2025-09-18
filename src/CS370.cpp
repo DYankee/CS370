99fc724006ebaf44a9f452752d1c4fed2bfe3bb9
Nick <147349946+NickD456@users.noreply.github.com>
2025-09-18 10:12:23 -0400

    Added in box that moves

--- src/CS370.cpp
+++ src/CS370.cpp
@@ -9,11 +9,21 @@ using namespace std;
 int main()
 {
 	// Create the main window	
-	const int screenWidth = 1200;
-	const int screenHeight = 800;
+	const int screenWidth = 1920;
+	const int screenHeight = 1080;
 	InitWindow(screenWidth, screenHeight, "CS370");
+	ToggleFullscreen();
 
+	
 	//init variables
+
+	 // Box properties
+    Vector2 boxPosition = {400, 300};   // Start in middle
+    Vector2 boxSize = {50, 50};         // Width & height
+    float speed = 400.0f;               // Pixels per second
+
+
+	/* ball
 	Texture2D ball = LoadTexture("../assets/cool-sports-ball.png");
 
     int frameWidth = ball.width;
@@ -29,20 +39,39 @@ int main()
     Vector2 origin = { destRec.width/2, destRec.height/2 };
 
     int rotation = 0;
+	*/
 
 	// Main game loop
     SetTargetFPS(60);
 	while (!WindowShouldClose()) // Detect window close button or ESC key
 	{
 
+		 float dt = GetFrameTime(); // Time since last frame
+
+		// Move box based on key input
+		if (IsKeyDown(KEY_D)) boxPosition.x += speed * dt;
+		if (IsKeyDown(KEY_A)) boxPosition.x -= speed * dt;
+		if (IsKeyDown(KEY_W)) boxPosition.y -= speed * dt;
+		if (IsKeyDown(KEY_S)) boxPosition.y += speed * dt;
+
+		// Constrain box to stay within screen bounds
+		if (boxPosition.x < 0) boxPosition.x = 0;
+		if (boxPosition.y < 0) boxPosition.y = 0;
+		if (boxPosition.x > screenWidth - boxSize.x) boxPosition.x = screenWidth - boxSize.x;
+		if (boxPosition.y > screenHeight - boxSize.y) boxPosition.y = screenHeight - boxSize.y;
+
 		// Update
-		rotation++;
+		//rotation++;
 
 		BeginDrawing();
 
 		ClearBackground(RAYWHITE);
 
-		DrawTexturePro(ball, sourceRec, destRec, origin, (float)rotation, GREEN);
+		
+		 DrawRectangleV(boxPosition, boxSize, BLUE); // Draw the box
+
+         DrawText("Move with W A S D", 10, 10, 20, BLACK);
+		//DrawTexturePro(ball, sourceRec, destRec, origin, (float)rotation, GREEN);
 
 		EndDrawing();
 	}
