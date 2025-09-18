45fd239320755e0176990d67bb5fb7e34b8bb2a5
Nick <147349946+NickD456@users.noreply.github.com>
2025-09-18 09:35:40 -0400

    Updated tasks.json
    Updated tasks.json to find and include the raylib libraries when compiling the program

--- src/CS370.cpp
+++ src/CS370.cpp
@@ -3,8 +3,69 @@
 
 #include <iostream>
 #include "../include/raylib.h"
+#include "../include/entt.hpp"
+
+
+#define CHAR_WIDTH 32
+#define CHAR_HEIGHT 64
 
 using namespace std;
+
+struct My_Transform {
+	Vector2 position;
+	Vector2 size;
+	float rotation;
+};
+struct My_Texture {
+	Texture2D texture;
+   	Rectangle sourceRec;
+
+	My_Texture(std::string filePath, int frameW, int frameH){
+		// Default constructor
+		texture = LoadTexture(filePath.c_str());
+		sourceRec = { 0.0f, 0.0f, (float)frameW, (float)frameH};
+	}
+};
+
+void update(entt::registry& registry, float dt) {
+
+	// Update all entities with Transform and Texture components
+	registry.view<My_Transform>().each([dt](auto& transform) {
+		// Example update logic: Move the entity to the right
+		transform.position.x += 1.0f * dt; // Move right by 1 unit per update
+	});
+	// Move box based on key input
+	/*
+	if (IsKeyDown(KEY_D)) boxPosition.x += speed * dt;
+	if (IsKeyDown(KEY_A)) boxPosition.x -= speed * dt;
+	if (IsKeyDown(KEY_W)) boxPosition.y -= speed * dt;
+	if (IsKeyDown(KEY_S)) boxPosition.y += speed * dt;
+	
+	// Constrain box to stay within screen bounds
+	if (boxPosition.x < 0) boxPosition.x = 0;
+	if (boxPosition.y < 0) boxPosition.y = 0;
+	if (boxPosition.x > screenWidth - boxSize.x) boxPosition.x = screenWidth - boxSize.x;
+	if (boxPosition.y > screenHeight - boxSize.y) boxPosition.y = screenHeight - boxSize.y;
+	*/
+}
+
+void draw(entt::registry& registry) {
+	// Draw all entities with Transform and Texture components
+	registry.view<My_Transform, My_Texture>().each([](auto& transform, auto& texture) {
+		// Draw the texture at the entity's position
+		DrawTexturePro(texture.texture,
+			texture.sourceRec,
+			Rectangle{
+				transform.position.x, transform.position.y,
+				transform.size.x, transform.size.y
+			},
+			{0.0f, 0.0f},
+			transform.rotation,
+			BLACK);
+	});
+}
+
+
 
 int main()
 {
@@ -14,63 +75,47 @@ int main()
 	InitWindow(screenWidth, screenHeight, "CS370");
 	ToggleFullscreen();
 
-	
-	//init variables
 
-	 // Box properties
-    Vector2 boxPosition = {400, 300};   // Start in middle
-    Vector2 boxSize = {50, 50};         // Width & height
+	//init entt registry
+	entt::registry registry;
+
+
+
+	//init test entities
+	for (size_t i = 0; i < 10; i++)
+	{
+		entt::entity entity = registry.create();
+		registry.emplace<My_Transform>(entity, Vector2{float(i * 50), float(i * 50)}, Vector2{CHAR_WIDTH, CHAR_HEIGHT}, 0.0f);
+		registry.emplace<My_Texture>(entity, "../../../assets/advnt_full.png", CHAR_WIDTH, CHAR_HEIGHT);
+	}
+
+
+	// Box properties
+	/*
+	Vector2 boxPosition = {400, 300};   // Start in middle
+    Vector2 boxSize = {32, 64};         // Width & height
     float speed = 400.0f;               // Pixels per second
-
-
-	/* ball
-	Texture2D ball = LoadTexture("../assets/cool-sports-ball.png");
-
-    int frameWidth = ball.width;
-    int frameHeight = ball.height;
-
-    // Source rectangle (part of the texture to use for drawing)
-    Rectangle sourceRec = { 0.0f, 0.0f, (float)frameWidth, (float)frameHeight};
-
-    // Destination rectangle (screen rectangle where drawing part of texture)
-    Rectangle destRec = { screenWidth/2.0f, screenHeight/2.0f, frameWidth/2.0f, frameHeight/2.0f };
-
-    // Origin of the texture (rotation/scale point), it's relative to destination rectangle size
-    Vector2 origin = { destRec.width/2, destRec.height/2 };
-
-    int rotation = 0;
 	*/
 
+	
 	// Main game loop
     SetTargetFPS(60);
 	while (!WindowShouldClose()) // Detect window close button or ESC key
 	{
+		float dt = GetFrameTime(); // Time since last frame
 
-		 float dt = GetFrameTime(); // Time since last frame
+		update(registry, dt);
 
-		// Move box based on key input
-		if (IsKeyDown(KEY_D)) boxPosition.x += speed * dt;
-		if (IsKeyDown(KEY_A)) boxPosition.x -= speed * dt;
-		if (IsKeyDown(KEY_W)) boxPosition.y -= speed * dt;
-		if (IsKeyDown(KEY_S)) boxPosition.y += speed * dt;
-
-		// Constrain box to stay within screen bounds
-		if (boxPosition.x < 0) boxPosition.x = 0;
-		if (boxPosition.y < 0) boxPosition.y = 0;
-		if (boxPosition.x > screenWidth - boxSize.x) boxPosition.x = screenWidth - boxSize.x;
-		if (boxPosition.y > screenHeight - boxSize.y) boxPosition.y = screenHeight - boxSize.y;
-
-		// Update
+				// Update
 		//rotation++;
 
 		BeginDrawing();
-
+		draw(registry);
 		ClearBackground(RAYWHITE);
 
 		
-		 DrawRectangleV(boxPosition, boxSize, BLUE); // Draw the box
-
-         DrawText("Move with W A S D", 10, 10, 20, BLACK);
+		//DrawRectangleV(boxPosition, boxSize, BLUE); // Draw the box
+        //DrawText("Move with W A S D", 10, 10, 20, BLACK);
 		//DrawTexturePro(ball, sourceRec, destRec, origin, (float)rotation, GREEN);
 
 		EndDrawing();
