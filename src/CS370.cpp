f4218a816cc6a5ff3c608f47899485586286a20d
Zachary <zpgeary@gmail.com>
2025-09-21 08:04:09 -0400

    finished demo showing the use of entt to manage the relationship between 3 components and their related entity

--- src/CS370.cpp
+++ src/CS370.cpp
@@ -2,13 +2,86 @@
 #include <iostream>
 #include "../include/raylib.h"
 
-#define CHAR_WIDTH 32
-#define CHAR_HEIGHT 64
-
-#define RAYTMX_IMPLEMENTATION
-#include "../include/raytmx.h"
+#define CHAR_WIDTH 32.0f
+#define CHAR_HEIGHT 64.0f
 
 using namespace std;
+
+struct My_Texture {
+	Texture2D texture;
+	float width;
+	float height;
+   	Rectangle sourceRec;
+
+	My_Texture(std::string filePath, float frameW, float frameH){
+		// Default constructor
+		texture = LoadTexture(filePath.c_str());
+		width = frameW;
+		height = frameH;
+		sourceRec = { 0.0f, 0.0f, frameW, frameH};
+	}
+};
+
+struct Transform2D {
+	Vector2 translation;    // Translation
+	Vector2 scale;          // Scale
+	float rotation;			// Rotation
+
+	Transform2D(Vector2 pos, Vector2 scl, float rot) {
+		translation = pos;
+		scale = scl;
+		rotation = rot;
+	}
+};
+
+struct Stats{
+	float moveSpeed = 0;
+
+	Stats(float speed) {
+		moveSpeed = speed;
+	}
+};
+
+void update(entt::registry& registry, float dt) {
+
+	// Update all entities with Transform and Texture components
+	registry.view<Transform2D, Stats>().each([dt](auto& transform, auto& stats) {
+		// Example update logic: Move the entity to the right
+		transform.translation.x += stats.moveSpeed * dt; // Move right by 1 unit per update
+	});
+
+	// Move box based on key input
+	/*
+	if (IsKeyDown(KEY_D)) boxPosition.x += speed * dt;
+	if (IsKeyDown(KEY_A)) boxPosition.x -= speed * dt;
+	if (IsKeyDown(KEY_W)) boxPosition.y -= speed * dt;
+	if (IsKeyDown(KEY_S)) boxPosition.y += speed * dt;
+	
+	// Constrain box to stay within screen bounds
+	if (boxPosition.x < 0) boxPosition.x = 0;
+	if (boxPosition.y < 0) boxPosition.y = 0;
+	if (boxPosition.x > screenWidth - boxSize.x) boxPosition.x = screenWidth - boxSize.x;
+	if (boxPosition.y > screenHeight - boxSize.y) boxPosition.y = screenHeight - boxSize.y;
+	*/
+}
+
+void draw(entt::registry& registry) {
+	// Draw all entities with Transform and Texture components
+	registry.view<Transform2D, My_Texture>().each([](auto& transform, auto& texture) {
+		// Draw the texture at the entity's position
+		DrawTexturePro(texture.texture,
+			texture.sourceRec,
+			Rectangle{
+				transform.translation.x, transform.translation.y,
+				texture.width, texture.height
+			},
+			{0.0f, 0.0f},
+			transform.rotation,
+			BLACK);
+	});
+}
+
+
 
 int main()
 {
@@ -20,16 +93,22 @@ int main()
 	InitWindow(screenWidth, screenHeight, "CS370");
 	ToggleFullscreen();
 
-    // Load TMX map
-    TmxMap* stage1 = LoadTMX("../assets/tiled/stage1.tmx");
-    if (!stage1) {
-        cout << "Failed to load stage1.tmx" << endl;
-        CloseWindow();
-        return -1;
-    }
+
+	//init entt registry
+	entt::registry registry;
 	
-	//init variables
-	const float gravity = 1000.0f;
+	//init test entities
+	for (size_t i = 0; i < 10; i++)
+	{
+		//create a new entity
+		entt::entity entity = registry.create();
+		//entity is a reference to the entity we just created
+		//All other arguments are passed to the component constructor
+		registry.emplace<Transform2D>(entity, Vector2{float(i * 50), float(i * 50)}, Vector2{CHAR_WIDTH, CHAR_HEIGHT}, 0.0f);
+		registry.emplace<My_Texture>(entity, "../../../assets/advnt_full.png", CHAR_WIDTH, CHAR_HEIGHT);
+		registry.emplace<Stats>(entity, 10.0f * i); //move speed increases with each entity
+	}
+
 
 	 // Box properties
     Vector2 boxPosition = {400.0f, 300.0f};   // Start in middle
@@ -65,48 +144,15 @@ int main()
 
 		float dt = GetFrameTime(); // Time since last frame
 
-		boxVol.y += gravity * dt; // Update volocity based on gravity
+		// Update
+		update(registry, dt);
 
-		// Move box based on key input
-		if (IsKeyPressed(KEY_SPACE)) // if player hits space jump 
-		{
-   			boxVol.y = jumpStrength; // player jumps using jump strength
-		}
-
-		if (IsKeyDown(KEY_D)) boxPosition.x += speed * dt; // move left
-		if (IsKeyDown(KEY_A)) boxPosition.x -= speed * dt; // move right
-
-		boxPosition.y += boxVol.y * dt; // update player position based on volocity
-
-		// Constrain box to stay within screen bounds
-		if (boxPosition.x < 0) boxPosition.x = 0;
-		if (boxPosition.y < 0) boxPosition.y = 0;
-		if (boxPosition.x > screenWidth - boxSize.x) boxPosition.x = screenWidth - boxSize.x;
-		if (boxPosition.y > screenHeight - boxSize.y) boxPosition.y = screenHeight - boxSize.y;
-
-		// Update cow texture position to follow the box
-		destRec.x = boxPosition.x;
-		destRec.y = boxPosition.y;
-
+		// Draw
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
-
-		// Draw background texture scaled to screen size
-		
-		/*DrawTexturePro(background, 
-			{0, 0, (float)background.width, (float)background.height}, 
-			{0, 0, (float)screenWidth, (float)screenHeight}, 
-			{0, 0}, 0, WHITE);*/
-
-		// Draw the map
-        DrawTMX(stage1, NULL, 0, 0, WHITE);
-
-		 //DrawRectangleV(boxPosition, boxSize, BLUE); // Draw the blue box
-
-		 DrawTexturePro(cow, sourceRec, destRec, origin, (float)rotation, WHITE);
-
-         DrawText("Move with W A S D", 10, 10, 20, BLACK);
-
+		//DrawRectangleV(boxPosition, boxSize, BLUE); // Draw the box
+        //DrawText("Move with W A S D", 10, 10, 20, BLACK);
+		//DrawTexturePro(ball, sourceRec, destRec, origin, (float)rotation, GREEN);
 		EndDrawing();
 	}
 
