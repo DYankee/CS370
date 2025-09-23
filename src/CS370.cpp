8a404ffc7441b2936bc5a34aa0db08990e49fbf6
Kent Palabon <palabok@sunypoly.edu>
2025-09-23 15:08:45 -0400

    commented out draw blue box and clear background lines

--- src/CS370.cpp
+++ src/CS370.cpp
@@ -74,14 +74,16 @@ int main()
 
 		BeginDrawing();
 
-		ClearBackground(RAYWHITE);
+		//ClearBackground(RAYWHITE);
 		// Draw background texture scaled to screen size
+		
 		DrawTexturePro(background, 
 			{0, 0, (float)background.width, (float)background.height}, 
 			{0, 0, (float)screenWidth, (float)screenHeight}, 
 			{0, 0}, 0, WHITE);
 
-		 DrawRectangleV(boxPosition, boxSize, BLUE); // Draw the blue box
+		 //DrawRectangleV(boxPosition, boxSize, BLUE); // Draw the blue box
+
 		 DrawTexturePro(cow, sourceRec, destRec, origin, (float)rotation, WHITE); // Draw cow over the box
 
          DrawText("Move with W A S D", 10, 10, 20, BLACK);
