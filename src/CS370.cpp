213d41a16759b46fe139eb1ef55f08e36ffa0825
Zachary <zpgeary@gmail.com>
2025-09-23 15:06:40 -0400

    Fixed merge mistake

--- src/CS370.cpp
+++ src/CS370.cpp
@@ -11,65 +11,6 @@
 
 using namespace std;
 
-/*
-struct My_Transform {
-	Vector2 position;
-	Vector2 size;
-	float rotation;
-};
-struct My_Texture {
-	Texture2D texture;
-   	Rectangle sourceRec;
-
-	My_Texture(std::string filePath, int frameW, int frameH){
-		// Default constructor
-		texture = LoadTexture(filePath.c_str());
-		sourceRec = { 0.0f, 0.0f, (float)frameW, (float)frameH};
-	}
-};
-*/
-
-void update(entt::registry& registry, float dt) {
-
-	// Update all entities with Transform and Texture components
-	registry.view<Transform>().each([dt](auto& transform) {
-		// Example update logic: Move the entity to the right
-		transform += 1.0f * dt; // Move right by 1 unit per update
-	});
-
-	// Move box based on key input
-	/*
-	if (IsKeyDown(KEY_D)) boxPosition.x += speed * dt;
-	if (IsKeyDown(KEY_A)) boxPosition.x -= speed * dt;
-	if (IsKeyDown(KEY_W)) boxPosition.y -= speed * dt;
-	if (IsKeyDown(KEY_S)) boxPosition.y += speed * dt;
-	
-	// Constrain box to stay within screen bounds
-	if (boxPosition.x < 0) boxPosition.x = 0;
-	if (boxPosition.y < 0) boxPosition.y = 0;
-	if (boxPosition.x > screenWidth - boxSize.x) boxPosition.x = screenWidth - boxSize.x;
-	if (boxPosition.y > screenHeight - boxSize.y) boxPosition.y = screenHeight - boxSize.y;
-	*/
-}
-
-void draw(entt::registry& registry) {
-	// Draw all entities with Transform and Texture components
-	registry.view<Transform, Texture>().each([](auto& transform, auto& texture) {
-		// Draw the texture at the entity's position
-		DrawTexturePro(texture.texture,
-			texture.sourceRec,
-			Rectangle{
-				transform.position.x, transform.position.y,
-				transform.size.x, transform.size.y
-			},
-			{0.0f, 0.0f},
-			transform.rotation,
-			BLACK);
-	});
-}
-
-
-
 int main()
 {
 	// Create the main window	
@@ -128,10 +69,7 @@ int main()
 		//rotation++;
 
 		BeginDrawing();
-		draw(registry);
 		ClearBackground(RAYWHITE);
-
-		
 		 DrawRectangleV(boxPosition, boxSize, BLUE); // Draw the box
 
          DrawText("Move with W A S D", 10, 10, 20, BLACK);
