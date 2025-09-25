58c22d0b17928dd9f32e9f3c3e238526de0b9d74
Kent Palabon <palabok@sunypoly.edu>
2025-09-25 09:09:50 -0400

    setup tiled assets

--- src/CS370.cpp
+++ src/CS370.cpp
@@ -9,6 +9,9 @@ extern "C" {
     #include "../include/raytmx.h"
 }
 
+#define RAYLIB_TILESON_IMPLEMENTATION
+#include "../include/raylib-tileson.h"
+
 using namespace std;
 
 int main()
@@ -30,10 +33,10 @@ int main()
 	float jumpStrength = -400.0f;       // Initial upward velocity
 
 	// Load cow texture
-	Texture2D cow = LoadTexture("../assets/cow.png");
+	Texture2D cow = LoadTexture("../assets/sprites/cow.png");
 
 	// Load background texture
-	Texture2D background = LoadTexture("../assets/bg.png");
+	Texture2D background = LoadTexture("../assets/sprites/bg.png");
 
     int frameWidth = cow.width;
     int frameHeight = cow.height;
