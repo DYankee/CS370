bdcced7f5b9f57abe266b4822f2b61c2fba18e98
Zachary <zpgeary@gmail.com>
2025-09-24 20:19:43 -0400

    removed old code

--- src/CS370.cpp
+++ src/CS370.cpp
@@ -158,16 +158,10 @@ int main()
 
 		// Draw
 		BeginDrawing();
-
-		//ClearBackground(RAYWHITE);
-		// Draw background texture scaled to screen size
-		
 		DrawTexturePro(background, 
 			{0, 0, (float)background.width, (float)background.height}, 
 			{0, 0, (float)screenWidth, (float)screenHeight}, 
 			{0, 0}, 0, WHITE);
-
-		 //DrawRectangleV(boxPosition, boxSize, BLUE); // Draw the blue box
 
 		 DrawTexturePro(cow, sourceRec, destRec, origin, (float)rotation, WHITE); // Draw cow over the box
 
