d734c7fea750048ac1f0bcdb22ca16dd878d70cb
Kent <palabok@sunypoly.edu>
2025-10-02 10:30:29 -0400

    working background. no parallax effect yet

--- assets/tiled/maintileset.tsx
+++ assets/tiled/maintileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="maintileset" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
- <image source="../sprites/tilesetmain.png" width="512" height="512"/>
+ <image source="../graphics/tileset/tilesetmain.png" width="512" height="512"/>
  <tile id="35">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
