b4003c58b0f80d5009b78f7491f7e1fb977f5b96
Zachary <zpgeary@gmail.com>
2025-10-16 13:54:45 -0400

    Started working on a player movement system

--- src/entities/player.hpp
+++ src/entities/player.hpp
@@ -5,12 +5,18 @@
 #include "../components/components.hpp"
 #include "../systems/systems.hpp"
 
+#define CHAR_WIDTH 32
+#define CHAR_HEIGHT 32
+#define SPEED 400.0f
+
 // Empty struct for entity tagging
 struct Player{
 };
 
 // Function to create a player entity
 // Takes in a pointer to an entt entity
-void createPlayer(entt::registry &registry);
+void createPlayer(entt::registry &registry, Camera2D camera);
+
+void playerMovementSystem(entt::registry &registry, float dt, float gravity);
 
 #endif
