06b6299ed09f82b2300739c78ae73cc5625fe70c
Zachary <zpgeary@gmail.com>
2025-10-17 05:04:32 -0400

    removed unesesary constructor

--- src/entities/map.cpp
+++ src/entities/map.cpp
@@ -1,17 +1,16 @@
 #include "map.hpp"
 
-// Map struct constructor
-Map::Map(){};
 
 // Function to create and setup a map entity
 void CreateMap(entt::registry &registry, std::string tmxFilePath, Music music) {
     entt::entity mapEntity = registry.create();
-    TmxMap* map = LoadTMX(tmxFilePath.c_str());
-    if (!map) {
+    TmxMap *mapPtr = LoadTMX(tmxFilePath.c_str());
+    if (!mapPtr) {
         TraceLog(LOG_ERROR, "Failed to load TMX map: %s", tmxFilePath.c_str());
         CloseWindow();
         return;
     }
+    TmxMap map = *mapPtr;
     registry.emplace<Map>(mapEntity, Map());
     registry.emplace<TmxMap>(mapEntity, map);
     registry.emplace<Music>(mapEntity, music);
