4a813fc0114c164dcc78971e60ab6cdbcb65d215
Zachary <zpgeary@gmail.com>
2025-10-17 01:56:40 -0400

    fixed function casing

--- src/components/sprite_data.cpp
+++ src/components/sprite_data.cpp
@@ -17,22 +17,22 @@
     }
 
     // Set source rectangle based on texture size
-    void SpriteData::setSrcRec(Texture2D texture) {
+    void SpriteData::SetSrcRec(Texture2D texture) {
         srcRec = {0, 0, (float)texture.width, (float)texture.height};
     }
 
     // Set the current texture by name
-    void SpriteData::setTexture(std::string name) {
+    void SpriteData::SetTexture(std::string name) {
         auto texture = textures.find(name);
         if (texture != textures.end()) {
             curentTexture = &texture->second;
-            setSrcRec(texture->second);
+            SetSrcRec(texture->second);
         } else {
             TraceLog(LOG_ERROR, "Texture '%s' not found!", name.c_str());
         }
     }
 
-    void SpriteData::addTexture(std::string name, std::string path) {
+    void SpriteData::AddTexture(std::string name, std::string path) {
         Texture2D texture = LoadTexture(path.c_str());
         if (texture.id != 0) { // Check if texture loaded successfully
             textures[name] = texture;
