085e2286b0c2956e7c3dbc1ec2e1ebe087a77a58
Zachary <zpgeary@gmail.com>
2025-10-17 01:56:19 -0400

    Moved functions for interacting with entities to the tag struct

--- src/entities/map.cpp
+++ src/entities/map.cpp
@@ -2,3 +2,25 @@
 
 // Map struct constructor
 Map::Map(){};
+
+// Function to create and setup a map entity
+void Map::CreateMap(entt::registry &registry, std::string tmxFilePath, Music music) {
+    entt::entity mapEntity = registry.create();
+    TmxMap* map = LoadTMX(tmxFilePath.c_str());
+    if (!map) {
+        TraceLog(LOG_ERROR, "Failed to load TMX map: %s", tmxFilePath.c_str());
+        CloseWindow();
+        return;
+    }
+    registry.emplace<Map>(mapEntity, Map());
+    registry.emplace<TmxMap>(mapEntity, map);
+    registry.emplace<Music>(mapEntity, music);
+}
+
+
+// Function to update map state
+void Map::UpdateMap(entt::registry &registry, float dt) {
+    registry.view<Map, TmxMap, Music>().each([dt](TmxMap &tmxMap, Music &music) {
+        UpdateMusicStream(music);
+    });
+}
--- src/entities/map.hpp
+++ src/entities/map.hpp
@@ -1,6 +1,20 @@
 #ifndef MAP_HPP
 #define MAP_HPP
+#include "../../include/entt.hpp"
+#include "../../include/raylib.h"
+#include "../../include/raytmx.h"
 
-struct Map{};
+// Map component for tagging map entities and storing functions that operate on them
+struct Map{
+    
+    // Function to create and setup a map entity
+    void CreateMap(entt::registry &registry, std::string tmxFilePath, Music music);
+    
+    // Function to update map state
+    void UpdateMap(entt::registry &registry, float dt);
+};
+
+
+
 
 #endif // MAP_HPP
