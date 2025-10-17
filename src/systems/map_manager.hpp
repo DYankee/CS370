a37715e2a12ebf25c58d07ed446cb54bf08c051b
Zachary <zpgeary@gmail.com>
2025-10-17 05:03:09 -0400

    cleaned up includes

---
+++ src/systems/map_manager.hpp
@@ -0,0 +1,12 @@
+#ifndef MAP_MANAGER_HPP
+#define MAP_MANAGER_HPP
+
+#include "../../include/entt.hpp"
+#include "../../include/raylib.h"
+#include "../components/components.hpp"
+#include "../entities/entities.hpp"
+
+void CheckForMapChange(entt::registry &registry);
+void ChangeMap(entt::registry &registry, std::string tmxFilePath, Vector2 startPosition); 
+
+#endif
