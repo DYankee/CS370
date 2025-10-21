f86c4c4d1b48b9a79350d06869787fc6dacc1992
Zachary <zpgeary@gmail.com>
2025-10-20 20:07:18 -0400

    added a camera controller

---
+++ src/systems/camera_controller.hpp
@@ -0,0 +1,10 @@
+#ifndef CAMERA_CONTROLLER_HPP
+#define CAMERA_CONTROLLER_HPP
+
+#include "../../include/entt.hpp"
+#include "../entities/entities.hpp"
+
+void CameraUpdate(entt::registry &registry, float dt);
+
+
+#endif // CAMERA_CONTROLLER_HPP
