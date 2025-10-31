18c463505b87387d64c1c5a0c6325e088087d731
Zachary <zpgeary@gmail.com>
2025-10-31 07:17:26 -0400

    Player now spawns at the spawn point designated in the map

--- src/systems/map_manager.cpp
+++ src/systems/map_manager.cpp
@@ -3,11 +3,11 @@
 int mapIndex = 1;
 
 // Change the current map to a new TMX file and set player start position
-void ChangeMap(entt::registry &registry, std::string tmxFilePath, Vector2 startPosition) {
-    TraceLog(LOG_TRACE, "Entering Function: ChangingMap");
+void ChangeMap(entt::registry &registry, std::string tmxFilePath) {
+    TraceLog(LOG_TRACE, "Entering Function: ChangeMap");
     TraceLog(LOG_INFO, "Changing map to: %s", tmxFilePath.c_str());
 
-    registry.view<Map, TmxMap>().each([&registry, &tmxFilePath, &startPosition](TmxMap &currentMap) {
+    registry.view<Map, TmxMap>().each([&registry, &tmxFilePath](TmxMap &currentMap) {
         // Load new map
         TmxMap* newMapPtr = LoadTMX(tmxFilePath.c_str());
         if (!newMapPtr) {
@@ -22,9 +22,16 @@ void ChangeMap(entt::registry &registry, std::string tmxFilePath, Vector2 startP
         currentMap = *newMapPtr;
         
         // Set player start position
-        registry.view<Player, Transform>().each([&startPosition, &registry](Transform &transform) {
-            transform.translation.x = startPosition.x;
-            transform.translation.y = startPosition.y;
+        registry.view<Player, Transform>().each([&registry, &currentMap](Transform &transform) {
+
+            TmxObjectGroup entities = FindLayerByName(currentMap.layers, currentMap.layersLength, "Entities")->exact.objectGroup;
+            TmxObject player = FindObjectByName(entities.objects, entities.objectsLength, "Player");
+
+            TraceLog(LOG_INFO, "Moving player to map spawn at: %f,%f", player.x, player.y);
+            transform.translation.x = player.x;
+            transform.translation.y = player.y;
+
+            TraceLog(LOG_INFO, "Player new location: %f,%f", transform.translation.x, transform.translation.y);
 
             registry.view<PlayerCamera, Camera2D>().each([&transform](Camera2D &camera){
                 SetCameraPos(camera, transform);
@@ -45,7 +52,7 @@ void CheckForMapChange(entt::registry &registry){
             if (playerPos.x < 0) {
                 mapIndex--;
                 if(mapIndex >= 1) {
-                    ChangeMap(registry, "assets/tiled/stage" + std::to_string(mapIndex)+ ".tmx", {100.0f, 100.0f});
+                    ChangeMap(registry, "assets/tiled/stage" + std::to_string(mapIndex)+ ".tmx");
                 }
                 else {
                     mapIndex = 2; // just goes back to stage1
@@ -55,7 +62,7 @@ void CheckForMapChange(entt::registry &registry){
             // Change to next stage if player goes past right edge
             if (playerPos.x > 1920) {
                 mapIndex++;
-                ChangeMap(registry, "assets/tiled/stage" + std::to_string(mapIndex) + ".tmx", {100.0f, 100.0f});
+                ChangeMap(registry, "assets/tiled/stage" + std::to_string(mapIndex) + ".tmx");
             }
         });
     });    
