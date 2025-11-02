2e506b50b2c127aae0f59a5d4c82a64d61c8bd14
Zachary <zpgeary@gmail.com>
2025-11-02 05:55:34 -0500

    Added logging

--- src/systems/enemy_controller.cpp
+++ src/systems/enemy_controller.cpp
@@ -2,14 +2,17 @@
 
 // Function to find enemy spawn point and spawn the designated enemy at them
 void SpawnEnemies(entt::registry &registry){
+    TraceLog(LOG_TRACE, "Entering function: SpawnEnemies");
     registry.view<Map, TmxMap>().each([&registry](TmxMap &map){
         //get the entities group from the map
         TmxObjectGroup entities = FindLayerByName(map.layers, map.layersLength, "Entities")->exact.objectGroup;
         // Get the enemySpawns form the entities list
         std::vector<TmxObject> enemySpawns = FindObjectsByType(entities.objects, entities.objectsLength, "Enemy");
         
+
         //loop through entity spawn
         for (TmxObject spawn : enemySpawns){
+            TraceLog(LOG_INFO, "Creating enemy at: %f,%f", float(spawn.x), float(spawn.y));
             CreateEnemy(registry, Vector2{float(spawn.x), float(spawn.y)});
         }
     });
