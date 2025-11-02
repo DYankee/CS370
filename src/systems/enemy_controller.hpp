7535af43a0e5d705c714db6c5fa7fec01476618c
Zachary <zpgeary@gmail.com>
2025-11-02 07:07:48 -0500

    added function to manage updating enemies

--- src/systems/enemy_controller.hpp
+++ src/systems/enemy_controller.hpp
@@ -6,9 +6,12 @@
 #include "../../include/raytmx.h"
 #include "../components/components.hpp"
 #include "../entities/entities.hpp"
+#include "systems.hpp"
 
 void SpawnEnemies(entt::registry &registry);
 
 void DeSpawnEnemies(entt::registry &registry);
+
+void UpdateEnemies(entt::registry &registry, float dt);
 
 #endif
