9c2776572cc74e962cf1d2f002ef18ba8ab49816
Zachary <zpgeary@gmail.com>
2025-11-02 04:39:51 -0500

    added test for finding objects by type

--- tests/test_controller.cpp
+++ tests/test_controller.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "asset_tests.cpp"
 #include <gtest/gtest.h>
+#include "../src/systems/map_manager.hpp"
 
 
 class AssetDataFixture : public ::testing::Test {
@@ -26,3 +27,26 @@ TEST_F(AssetDataFixture, TextureLoading){
 //TEST_F(AssetDataFixture, MapLoading){
 //    EXPECT_TRUE(LoadMapTest(assets.maps));
 //}
+
+/*
+Tests for functions that act on maps
+*/ 
+// Make sure the correct number of objects are found
+TEST(MapFunctions, FindObjTest){
+    InitWindow(1,1,"Tests");
+    TmxMap* map = LoadTMX("tiled/stage3.tmx");
+    TmxObjectGroup entities;
+    std::vector<TmxObject> EnemyObjs;
+    int EnemiesFound = 0;
+    if (!map) {
+        EnemiesFound = -1;
+    }
+    else{
+        TmxObjectGroup entities = FindLayerByName(map->layers, map->layersLength, "Entities")->exact.objectGroup;
+        EXPECT_EQ(entities.objectsLength, 3);
+        std::vector<TmxObject> EnemyObjs = FindObjectsByType(entities.objects, entities.objectsLength, "Enemy");
+        EnemiesFound = EnemyObjs.size();
+    }
+    EXPECT_EQ(EnemiesFound, 2);
+    CloseWindow();
+}
