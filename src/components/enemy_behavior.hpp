facfea58a5dfdeeeed6c0cb7e57d40e6b6a3a29b
Zachary <zpgeary@gmail.com>
2025-11-03 06:13:09 -0500

    Added registry as argument for enemy update function

--- src/components/enemy_behavior.hpp
+++ src/components/enemy_behavior.hpp
@@ -1,17 +1,17 @@
 #ifndef ENEMY_BEHAVIOR_HPP
 #define ENEMY_BEHAVIOR_HPP
 
+#include "../../include/entt.hpp"
 #include "../../include/raylib.h"
 #include "enemy_stats.hpp"
 
-typedef void (*UpdateFunction)(float, EnemyStats&, Transform&);
-
+typedef void (*UpdateFunction)(entt::registry&, float, EnemyStats&, Transform&);
 struct Enemy_behavior{
     UpdateFunction Update;
 
     Enemy_behavior(UpdateFunction Update);
 };
 
-void TestUpdateFunc(float, EnemyStats&, Transform&);
+void TestUpdateFunc(entt::registry&, float, EnemyStats&, Transform&);
 
 #endif
