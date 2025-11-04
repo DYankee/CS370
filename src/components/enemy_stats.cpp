c9fc6e4cb0268ce148aa0886aa904068c6120e5a
Zachary <zpgeary@gmail.com>
2025-11-03 19:04:10 -0500

    Added Direction enum

--- src/components/enemy_stats.cpp
+++ src/components/enemy_stats.cpp
@@ -1,9 +1,22 @@
 #include "enemy_stats.hpp"
 
-EnemyStats::EnemyStats(int enemyHealth, int enemyMaxHealth, float enemySpeed, float dmg, bool followsPlayer) {
+
+Direction ChangeDirection(Direction d){
+    Direction direction;
+    if(d == LEFT){
+        direction = RIGHT;
+    }
+    else if (d == RIGHT){
+        direction = LEFT;
+    }
+    return direction;
+}
+
+EnemyStats::EnemyStats(int enemyHealth, int enemyMaxHealth, float enemySpeed, float dmg, Direction direction, bool followsPlayer) {
     this->enemyHealth = enemyMaxHealth;
     this->enemyMaxHealth = enemyMaxHealth;
     this->enemySpeed = enemySpeed;
     this->dmg = dmg;
+    this->CurrentDirection = direction;
     this->followsPlayer = followsPlayer;
 }
--- src/components/enemy_stats.hpp
+++ src/components/enemy_stats.hpp
@@ -1,14 +1,19 @@
 #ifndef ENEMY_STATS_HPP
 #define ENEMY_STATS_HPP
 
-struct EnemyStats {
-    int enemyHealth;    //enemy's current health
-    int enemyMaxHealth; //enemy's maxiumum health
-    float enemySpeed;   //enemy's movement speed
-    float dmg;          //enemy's dmg to the player
-    bool followsPlayer; //determines whether enemy follows the player or not
+enum Direction {LEFT, RIGHT};
 
-    EnemyStats(int enemyHealth, int enemyMaxHealth, float enemySpeed, float dmg, bool followsPlayer);
+Direction ChangeDirection(Direction);
+
+struct EnemyStats {
+    int enemyHealth;            //enemy's current health
+    int enemyMaxHealth;         //enemy's maxiumum health
+    float enemySpeed;           //enemy's movement speed
+    float dmg;                  //enemy's dmg to the player
+    Direction CurrentDirection; //enemy's Current direction
+    bool followsPlayer;         //determines whether enemy follows the player or not
+
+    EnemyStats(int enemyHealth, int enemyMaxHealth, float enemySpeed, float dmg, Direction direction, bool followsPlayer);
 };
 
 #endif
