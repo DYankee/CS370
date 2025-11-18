8e01a9047e9dd40f5e658ce28fc07d2c478bba4a
Zachary <zpgeary@gmail.com>
2025-11-18 01:09:11 -0500

    Added framework for projectiles

--- src/entities/projectile.cpp
+++ src/entities/projectile.cpp
@@ -1,7 +1,20 @@
 #include "projectile.hpp"
+#include "../components/components.hpp"
 
-void CreateProjectile(entt:: registry registry, Transform startPos, Vector2 Direction){
+void CreateProjectile(entt:: registry registry, Transform startPos, Vector2 direction, ProjectileStats stats){
+    TraceLog(LOG_TRACE, "Entering function: Create projectile");
+
     entt::entity projectile = registry.create();
-
     
+    // Add tag struct
+    registry.emplace<Projectile>(projectile, Projectile{});
+
+    // Set start pos
+    registry.emplace<Transform>(projectile, startPos);
+
+    // Set Direction
+    registry.emplace<Vector2>(projectile, direction);
+
+    // Set Stats
+    registry.emplace<ProjectileStats>(projectile, stats);
 }
---
+++ src/systems/projectile_manager.hpp
@@ -0,0 +1,12 @@
+#ifndef PROJECTILE_MANAGER_HPP
+#define PROJECTILE_MANAGER_HPP
+
+#include "../../include/entt.hpp"
+#include "../../include/raylib.h"
+
+
+// Function to update all projectiles
+void UpdateProjectiles(entt::registry&, float);
+
+
+#endif
