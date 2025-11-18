7e1ada5c4dc4150e5bc16be01ed7cff60710e1fa
Zachary <zpgeary@gmail.com>
2025-11-18 00:34:47 -0500

    Started working on projectile system

---
+++ src/entities/projectile.hpp
@@ -0,0 +1,12 @@
+#ifndef PROJECTILE_HPP
+#define PROJECTILE_HPP
+
+#include "../../include/entt.hpp"
+#include "../../include/raylib.h"
+
+struct projectile{};
+
+void CreateProjectile(entt::registry, Transform, Vector2);
+
+
+#endif
