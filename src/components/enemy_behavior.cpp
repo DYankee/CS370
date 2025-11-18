8120d3c5225d2a1750ebc93432f3983ea4902e1b
Kent Palabon <palabok@sunypoly.edu>
2025-11-18 14:49:34 -0500

    farmer sprites now move left and right

--- src/components/enemy_behavior.cpp
+++ src/components/enemy_behavior.cpp
@@ -33,6 +33,7 @@ void BasicEnemyUpdate(entt::registry & registry, float dt, entt::entity enemy){
     auto& physics = registry.get<PhysicsObject>(enemy);
     auto& stats = registry.get<EnemyStats>(enemy);
     auto& spawn = registry.get<Vector2>(enemy);
+    auto& sprite = registry.get<SpriteData>(enemy);
 
     physics.velocity.y += 2000.0f * dt;
 
