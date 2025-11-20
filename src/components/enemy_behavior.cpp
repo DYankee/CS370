3e5f894de6cd2ddf6b2f32695abe225cb04a4d3a
Zachary <zpgeary@gmail.com>
2025-11-20 13:06:23 -0500

    reworked collision function

--- src/components/enemy_behavior.cpp
+++ src/components/enemy_behavior.cpp
@@ -19,7 +19,7 @@ void TestUpdateFunc(entt::registry &registry, float dt, entt::entity enemy){
 void BasicEnemyUpdate(entt::registry & registry, float dt, entt::entity enemy){
     TraceLog(LOG_TRACE, "Entering Function BasicEnemyUpdate");
     TraceLog(LOG_INFO, "Updating entity: %d", enemy);
-    float maxDistance = 100;    
+    float maxDistance = 300;    
 
 
     // Get components from enemy entity    auto& pos = registry.get<Transform>(enemy);
@@ -29,7 +29,7 @@ void BasicEnemyUpdate(entt::registry & registry, float dt, entt::entity enemy){
     auto& spawn = registry.get<Vector2>(enemy);
     auto& sprite = registry.get<SpriteData>(enemy);
 
-    physics.velocity.y += 2000.0f * dt;
+    physics.velocity.y += GRAVITY * dt;
 
     if(!stats.followsPlayer){
         int distanceFromSpawn = abs(pos.translation.x - spawn.x);
