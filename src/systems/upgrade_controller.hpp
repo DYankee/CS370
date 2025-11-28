63f397e0fb3d8a301d8b84c3f2dc10d5af1d19df
AlainaS9 <saboa@sunypoly.edu>
2025-11-28 11:52:24 -0500

    Upgrade checking system in progress

---
+++ src/systems/upgrade_controller.hpp
@@ -0,0 +1,16 @@
+#ifndef UPGRADE_CONTROLLER_HPP
+#define UPGRADE_CONTROLLER_HPP
+
+#include "../../include/entt.hpp"
+#include "../../include/raylib.h"
+#include "../components/components.hpp"
+#include "../components/health_pickup.hpp"
+
+
+void SpawnUpgrades(entt::registry &registry);
+void UpdateUpgrades(entt::registry &registry, float dt);
+
+void SpawnHealthUpgrades(entt::registry &registry);
+void UpdateHealthUpgrades(entt::registry &registry, float dt);
+
+#endif
