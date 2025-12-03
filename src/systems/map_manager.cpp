34f1c90be1ba6df0baa07d6988aa1361eafdf43e
Zachary <zpgeary@gmail.com>
2025-12-03 13:42:11 -0500

    Added function to reset the mapIndex

--- src/systems/map_manager.cpp
+++ src/systems/map_manager.cpp
@@ -152,6 +152,5 @@ std::map<std::string, float> GetObjectProperties(TmxObject object){
 };
 
 void ResetMapIndex(){
-    TraceLog(LOG_TRACE, "Entering Function: ResetMapIndex");
     mapIndex = 1;
 }
