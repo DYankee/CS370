ad1fd65f79f9f252251dfa2e1536d2ba884542fc
Zachary <zpgeary@gmail.com>
2025-12-03 13:41:26 -0500

    Added function to check for player death

--- src/systems/player_controller.hpp
+++ src/systems/player_controller.hpp
@@ -8,5 +8,6 @@
 
 void PlayerInputSystem(entt::registry &registry, float dt);
 void MovePlayer(entt::registry &registry, float dt, entt::entity entity);
+void CheckForDeath(entt::registry &registry);
 
 #endif
