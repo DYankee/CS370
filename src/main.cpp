5ab67832215fa2059c7bdbbaccde4c0ef135dc6a
Zachary <zpgeary@gmail.com>
2025-12-04 13:53:03 -0500

    finished reworking the music system

--- src/main.cpp
+++ src/main.cpp
@@ -244,8 +244,6 @@ int main() {
 
 
 
-    // Load sound effects
-    Sound titleMooSound = LoadSound("assets/audio/titleMoo.mp3");
 
     // Load title texture
     Texture2D titleTexture = LoadTexture("assets/graphics/title/milksong_logo.png");
@@ -298,6 +296,11 @@ int main() {
 
     // Music setup
     InitAudioDevice();
+    
+    // Load sound effects
+    Sound titleMooSound = LoadSound("assets/audio/titleMoo.mp3");
+    
+    // Load music and start title track
     CreateJukebox(registry);
     StartSong(registry, "title");
     
