0e120a1884f91064d568aaedc6903cf33ecb4088
Zachary <zpgeary@gmail.com>
2025-12-03 23:38:45 -0500

    Added function for loading music

--- src/systems/asset_loader.cpp
+++ src/systems/asset_loader.cpp
@@ -1,7 +1,6 @@
 #include "asset_loader.hpp"
 
 // Function to load multiple textures and store them in a map
-// Created by Zachary Geary
 std::map<std::string, Texture2D> LoadTextures(std::map<std::string, std::string> texturePaths){
     TraceLog(LOG_TRACE, "Entering Function: LoadTextures");
         
@@ -18,4 +17,24 @@ std::map<std::string, Texture2D> LoadTextures(std::map<std::string, std::string>
         }
     }
     return textures;
+}
+
+// Function to load multiple songs and store them in a map
+std::map<std::string, Music> LoadMusic(std::map<std::string, std::string> songPaths){
+    TraceLog(LOG_TRACE, "Entering Function: LoadMusic");
+        
+    std::map<std::string, Music> songs;
+    //Loop through the provided paths and load each texture into the map
+    for (const auto pair : songPaths) {
+        const std::string name = pair.first;
+        const std::string path = pair.second;
+        Music song = LoadMusicStream(path.c_str());
+        SetMusicVolume(song, 1.0f);
+        if (song.stream.buffer != NULL ) { // Check if texture loaded successfully
+            songs[name] = song;
+        } else {
+            TraceLog(LOG_ERROR, "Failed to load texture: %s", path.c_str());
+        }
+    }
+    return songs;
 }
